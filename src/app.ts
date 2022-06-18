d0262558d54195b5163caafe902075f14a7ef723
Piotr Setsko <lihadzey@gmail.com>
2022-06-18 23:51:55 +0300

    feature: add rest methods

--- src/app.ts
+++ src/app.ts
@@ -54,25 +54,12 @@ const server = http.createServer(async (req, res) => {
       res.end();
     } else if (req.url === '/api/users' && req.method === 'POST') {
       let body = '';
-
       req.on('data', (chunk) => {
         body = chunk;
       });
 
       req.on('end', () => {
-        if (!(validateData(body)) && !(Object.keys(body).length === 3)) {
-          res.statusCode = 400;
-          res.setHeader('content-Type', 'Application/json');
-          res.end('Wrong data type');
-        } else {
-          // const postBody: IUser = {
-          //   ...Object.assign(
-          //     ['username', 'age', 'hobbies'].map((key: string) => ({
-          //       [key]: JSON.parse(body)[key]
-          //     }))
-          //   )
-          // };
-
+        if (validateData(body, res)) {
           const postBody: IUser = { ...JSON.parse(body), id: uuidv4() };
 
           users.push(postBody);
@@ -88,7 +75,7 @@ const server = http.createServer(async (req, res) => {
           res.end(JSON.stringify(response));
         }
       });
-    } else if (req.url?.includes('/api/users/') && req.url.split('/').length === 4) {
+    } else if (req.url?.includes('/api/users/') && req.url.split('/').length === 4 && ['PUT', 'GET', 'DELETE'].includes(req.method as string)) {
       if (validateId(req.url, users, res)) {
         const actualUser: IUser = validateId(req.url, users, res) as IUser;
 
@@ -99,14 +86,38 @@ const server = http.createServer(async (req, res) => {
           );
           res.end();
         } else if (req.method === 'PUT') {
-          users.map((user: IUser) =>
-            ((user.id === actualUser.id)
-              ? actualUser
-              : user));
+          let body = '';
 
-          res.writeHead(200, { 'Content-Type': 'application/json' });
+          req.on('data', (chunk) => {
+            body = chunk;
+          });
+
+          req.on('end', () => {
+            if (validateData(body, res)) {
+              let updatedUser = {} as IUser;
+
+              users = users.map((user: IUser) => {
+                if (user.id === actualUser.id) {
+                  updatedUser = { ...JSON.parse(body), id: user.id };
+                  return updatedUser;
+                }
+                return user;
+              });
+
+              res.writeHead(200, { 'Content-Type': 'application/json' });
+              res.write(
+                `New user ${JSON.stringify(updatedUser)}`
+              );
+              res.end();
+            }
+          });
+        } else if (req.method === 'DELETE') {
+          users = users.filter((user: IUser) =>
+            (user.id !== actualUser.id));
+
+          res.writeHead(202, { 'Content-Type': 'application/json' });
           res.write(
-            `New user ${JSON.stringify(actualUser)}`
+            'Record is found and deleted'
           );
           res.end();
         }
@@ -120,7 +131,7 @@ const server = http.createServer(async (req, res) => {
     }
   } catch (error) {
     res.writeHead(500, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Problem on serverside' }));
+    res.end(JSON.stringify({ message: 'Problem on server side' }));
   }
 });
 
--- src/validateData.ts
+++ src/validateData.ts
@@ -1,7 +1,19 @@
-const validateData = (value: string) => {
+import http from 'http';
+
+const validateData = (value: string, res: http.ServerResponse) => {
   const obj = JSON.parse(value);
-  return ['username', 'age', 'hobbies'].every((prop: string) =>
-    Object.prototype.hasOwnProperty.call(obj, prop))
+  const isKeys = ['username', 'age', 'hobbies'].every((prop: string) =>
+    Object.prototype.hasOwnProperty.call(obj, prop));
+  const isObjectLength = Object.keys(obj).length === 3;
+
+  if (!isKeys && !isObjectLength) {
+    res.statusCode = 400;
+    res.setHeader('content-Type', 'Application/json');
+    res.end('Wrong data type');
+
+    return false;
+  }
+  return true;
 };
 
 export default validateData;
